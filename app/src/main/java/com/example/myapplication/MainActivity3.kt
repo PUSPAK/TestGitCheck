4c95eb2272a7e209a4b879d02a75b921c614b7c6
Puspak <pudasgupta@paypal.com>
2022-02-20 22:36:39 +0530

    Working on Feauture 2.0

--- app/src/main/java/com/example/myapplication/MainActivity3.kt
+++ app/src/main/java/com/example/myapplication/MainActivity3.kt
@@ -7,5 +7,9 @@ class MainActivity3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState : Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main3)
+
+    }
+    fun feature(){
+        System.out.println("Feauture Branch")
     }
 }
